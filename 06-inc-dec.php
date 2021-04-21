9508dfc25e6fa66ef5b6e57623678958c119c4f9
janethcj <78242681+janethcj@users.noreply.github.com>
2021-04-21 15:08:26 -0500

    Create 06-inc-dec.php
    -Incrementos
    -Decrementos

---
+++ 06-inc-dec.php
@@ -0,0 +1,22 @@
+/*
+@author: janethcj
+@description: -Incremento
+              -Decremento
+*/
+
+<?php include 'includes/header.php';
+
+//Incrementos
+$numero1 = 30 ;
+echo $numero1++;
+
+//Se incrementa en 5.
+$numero 1 += 5;
+echo $numero1;
+
+//Decrementos
+$numero2 = 30;
+echo $numero2--;
+
+
+include 'includes/footer.php';
