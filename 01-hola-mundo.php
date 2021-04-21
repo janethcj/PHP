74a15bc9f4f362b3824e9509f257b3183743f06f
janethcj <78242681+janethcj@users.noreply.github.com>
2021-04-21 14:30:34 -0500

    Create 01-hola-mundo.php
    -Introducción a las diferentes formas de mostrar texto en pantalla.
    -Uso de include

---
+++ 01-hola-mundo.php
@@ -0,0 +1,23 @@
+/*
+@author: janethcj
+@description:   -Introducción a php.
+                -Imprimir en pantalla
+                -ECHO conocido como construcción del lenguaje.
+*/
+
+<?php include 'includes/header.php';?>
+
+<?php
+  echo "Hola Mundo";
+ ?>
+
+<br>
+
+<?php echo ("Hola Mundo");
+
+  print("Hola Mundo");
+  print "Hola Mundo";
+  print_r("Hla Mundo");
+  var_dump("Hola Mundo");
+
+include 'includes/footer.php';?>
